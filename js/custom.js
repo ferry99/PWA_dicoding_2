ca826e57e60a4bf9701f2e3786c4769071a34f24
ferry <ferrysanjaya99@gmail.com>
2019-12-02 21:52:51 +0700

    add fav team

--- js/custom.js
+++ js/custom.js
@@ -5,6 +5,15 @@ var blog = {
 		this.initPage();
 		this.initSW();
 		this.eventHandler();
+		this.initIdb();
+	},
+
+	initIdb : function(){
+		var dbPromise = idb.open("mydatabase", 1, function(upgradeDb) {
+		if (!upgradeDb.objectStoreNames.contains("events")) {
+			upgradeDb.createObjectStore("events");
+			}
+		});
 	},
 
 	initSW:function(){
@@ -64,6 +73,9 @@ var blog = {
 		  var content = document.querySelector("#body-content");
 		  if (this.status == 200) {
 		    content.innerHTML = xhttp.responseText;
+		    if(pages == "list_competition"){
+		    	self.loadListCompetition();
+		    }
 		    self.reinit();
 		  } else if (this.status == 404) {
 		    content.innerHTML = "<p>Page not found.</p>";
@@ -91,6 +103,7 @@ var blog = {
 
 		$('body').on('click' , '#load-league' , function(){
 			idLeague = $("#select-league").val();
+			$(".preloader-wrapper").addClass("active");	
 			$.ajax({
 	            url: 'https://api.football-data.org/v2/competitions/'+idLeague+'/standings',
 	            type: 'GET',
@@ -100,9 +113,11 @@ var blog = {
 	            },
 	            success: function (result) {
 	            	self.renderTableStanding(result);
+    				$(".preloader-wrapper").removeClass("active");	
 	            },
 	            error: function (error) {
-	               alert('League Not Avaiable !')
+	               alert('League Not Avaiable !');
+           			$(".preloader-wrapper").removeClass("active");	
 	            }
         	});
 		})
@@ -111,7 +126,7 @@ var blog = {
 		$('body').on('click' , '.show-detail-team' , function(e){
 			e.preventDefault();
 			idTeam = $(this).attr("data-id");
-
+			$(".preloader-wrapper").addClass("active");	
 			$.ajax({
 	            url: 'https://api.football-data.org/v2/teams/'+idTeam,
 	            type: 'GET',
@@ -123,9 +138,11 @@ var blog = {
 	            	// console.log(result);
 	            	self.renderModalDetailTeam(result);
     				$("#detail-team-modal").modal("open");
+					$(".preloader-wrapper").removeClass("active");	
 	            },
 	            error: function (error) {
-	               alert('err !')
+	               alert('err !');
+           			$(".preloader-wrapper").removeClass("active");	
 	            }
         	});
 		})
@@ -148,7 +165,10 @@ var blog = {
 						<td>'+rowData['position']+'</td>\
 						<td>'+rowData['team_name']+'</td>\
 						<td>'+rowData['points']+'</td>\
-						<td><a href="" class="waves-effect waves-light btn show-detail-team" data-id="'+rowData['team_id']+'"><i style="margin-right:0px" class="material-icons left">remove_red_eye</i></a></td>\
+						<td>\
+							<a href="" class="waves-effect waves-light btn show-detail-team btn-small" data-id="'+rowData['team_id']+'"><i style="margin-right:0px" class="material-icons left">remove_red_eye</i></a>\
+							<a href="" class="waves-effect waves-light yellow darken-2 btn show-detail-team btn-small" data-id="'+rowData['team_id']+'"><i style="margin-right:0px" class="material-icons left">star_border</i></a>\
+						</td>\
 					</tr>';
 				i++;
 				tableData.push(rowData);
@@ -186,7 +206,68 @@ var blog = {
 		}catch(e){
 			alert(e);
 		}
+	},
+
+	renderTableCompetition: function(data){
+		try{
+			competitions  = data.competitions;	
+			tableData = []; rowData = []; tr = "";
+			$.each(competitions , function(key , row){
+				rowData['country_name'] = row.area.name;
+				rowData['competition_name'] = row.name;
+				// console.log(row.name);
+				if(row.currentSeason != null){	
+					rowData['start_date'] = (row.currentSeason.startDate == null) ? "-" : row.currentSeason.startDate;
+					rowData['end_date'] = (row.currentSeason.endDate == null) ? "-" : row.currentSeason.endDate;	
+					if(row.currentSeason.winner != null){
+						rowData['winner'] = (row.currentSeason.winner.name == null) ? "-" : row.currentSeason.winner.name;
+					}else{
+						rowData['winner'] = "-";						
+					}		
+				}else{
+					rowData['start_date'] = "-";
+					rowData['end_date'] = "-";
+					rowData['winner'] = "-";
+				}
+				
+				tr += 	'<tr>\
+						<td>'+rowData['country_name']+'</td>\
+						<td>'+rowData['competition_name']+'</td>\
+						<td>'+rowData['start_date']+'</td>\
+						<td>'+rowData['end_date']+'</td>\
+						<td>'+rowData['winner']+'</td>\
+					</tr>';
+				// i++;
+			})
+
+			$("#competition-table tbody").html(tr);
+			
+			// console.log(rowData);
+		}catch(e){
+			alert(e);
+		}
+	},
+
+	loadListCompetition : function(){	
+		$(".preloader-wrapper").addClass("active");	
+		$.ajax({
+            url: 'https://api.football-data.org/v2/competitions',
+            type: 'GET',
+            dataType: 'json',
+            headers: {
+                'X-Auth-Token': '41998c78d52e4bf588634a956b0aa17e'
+            },
+            success: function (result) {
+            	self.renderTableCompetition(result);
+           		$(".preloader-wrapper").removeClass("active");	
+            },
+            error: function (error) {
+               alert('err !')
+            }
+    	});
 	}
+
+
 }
 
 
